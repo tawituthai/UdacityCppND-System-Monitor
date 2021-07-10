2c0d5c8d036e6b1a80e46901cf29f938368df00d
tawituthai <iamtawit@gmail.com>
2021-07-10 11:03:49 +0800

    Fixed width and fill for Format::ElapsedTime

--- src/format.cpp
+++ src/format.cpp
@@ -1,16 +1,23 @@
 #include "format.h"
 
+#include <iomanip>
 #include <string>
 
 using std::string;
 
 string Format::ElapsedTime(long seconds) {
   int hours_, minutes_, seconds_;
-  string result;
+  std::string result;
+  std::stringstream ss;
+
   hours_ = int(seconds / 3600.0);
   minutes_ = int((seconds - (hours_ * 3600.0)) / 60.0);
   seconds_ = int(seconds - (hours_ * 3600.0) - (minutes_ * 60.0));
-  result = std::to_string(hours_) + ":" + std::to_string(minutes_) + ":" +
-           std::to_string(seconds_);
+
+  ss << std::setw(2) << std::setfill('0') << hours_ << ":"
+     << std::setw(2) << std::setfill('0') << minutes_ << ":" 
+     << std::setw(2) << std::setfill('0') << seconds_;
+
+  result = ss.str();
   return (result);
 }
